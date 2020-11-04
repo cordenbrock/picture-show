cfeb92c5e41486f2d5f7002aceeb279906756be6
cordenbrock <cordenbrock@gmail.com>
2020-11-03 16:29:01 -0800

    renders select time dropdown
    co-authored-by: shannon grantski <72650879+grantskis@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# TItle
+# Title
 
 
 ## Description
 
 
+
+## Specs
+- object constructor to create movies with a variety of properties.. such as:
+title
+lead actor
+rating (age restriction)
+release date
+necessry bribe to circumvent rating
+base ticket price
+backend secret title
+show times. could be an array.
+
+
+
+movies to include:
+hippos ate my parents
+honey i hippo'd the kids
+shakespeare's othippo
+
+
+
 ### Legal
 
-_Distributed under the MIT License, Copyright (c) 2020 Cory Nordenbrock_
+_Distributed under the MIT License_
--- js/scripts.js
+++ js/scripts.js
@@ -9,37 +9,45 @@ function Movie(title, leadAct, ageRestriction, basePrice, showTime) {
 }
 
 let hippoParents = new Movie("Hippos Ate My Parents", "Chip McHip", 18, 17.50, ["the witching hour", "dusk 'til dawn marathon"]);
-
 let honeyHippod = new Movie("Honey I Hippo'd the Kids", "Tamus Ojec", 5, 11, ["high noon", "the witching hour"]); 
-
 let shakespearesOthippo = new Movie("Shakespeare's Othippo", "Chip McHip", 13, 22, ["high noon", "dusk 'til dawn marathon"]);
+let movies = [hippoParents, honeyHippod, shakespearesOthippo];
+// console.log(movies[2].leadAct);
+Movie.prototype.bribery = function() { return this.basePrice + (this.ageRestriction / 2) + 2.25 }
 
-
-Movie.prototype.bribery = function() {
-  return this.basePrice + (this.ageRestriction / 2) + 2.25
-}
-
-// function CONSTRUCTOR-NAME(key1, key2, key3) {
-// 	this.key1 = key1;
-// 	this.key2 = key2;
-// 	this.key3 = key3;
-// }
 
 
 // UI logic
+function renderTimeSelectDropdown(movieTitle) {
+  // console.log(movieTitle)
+  for (let i = 0; i <= movies.length; i++) {
+    if (movies[i].title === movieTitle) {
+    // console.log(movies[i].showTime);
+    let showTimeOne = (movies[i].showTime)[0];
+    let showTimeTwo = (movies[i].showTime)[1];
+    $("#time-select").html(
+      `<option value="" disabled selected value>-- select a time --</option>
+      <option value="">${showTimeOne}</option>
+      <option value="">${showTimeTwo}</option>`
+      );
+    };
+  };
+};
+
 function addEventListeners() {
-  $("#movie-select").on("click", "option", function() {
-    console.log(this.id);
+  $(document).on('change', '#movie-select', function(e) {
+    let selectedMovie = this.options[e.target.selectedIndex].text;
+    renderTimeSelectDropdown(selectedMovie);
   });
 };
 
 addEventListeners();
 
 
+
+
 $(document).ready(function() {
   $("#form").submit(e => {
     e.preventDefault();
-
-
   })
 });
